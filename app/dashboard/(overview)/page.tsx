77469e7f7c489f3db0adf53b4a9078795d73ced5
SiyabongaMat <79463249+SiyabongaMat@users.noreply.github.com>
2024-09-19 17:31:16 +0200

    final

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,6 +1,5 @@
 import { Metadata } from 'next';
 import React from 'react';
-import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
